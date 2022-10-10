4b328a358f7c730e900c41f8ec377f8db17686ad
Jack Penzer <jack.penzer@sharecreative.com>
2022-10-10 09:42:48 +0100

    save plot list + count_multiple
    Small documentation upgrades + adding DPI argument to save plots list.

--- R/save_plot_list.R
+++ R/save_plot_list.R
@@ -1,11 +1,14 @@
-#' Save a list of plots
+#' Save a list of plots to the current working directory
+#'
+#' Set your working directory to the folder you want to save your plots to with `setwd("~/foo/bar")` then call the function on your list of plots.
 #'
 #' @param plot_list Should be a named list of plots
 #' @param plot_type For saving prefix that gets pasted with name and .png = e.g. "bigram_microsoft_topic_1.png"
+#' @param dpi Resolution of saved image - higher number = higher quality
 #'
 #' @return does not return anything - saves plots to working directory
 #' @export
 #'
-save_plot_list <- function(plot_list, plot_type = "bigram"){
-  lapply(names(plot_list),function(x) ggplot2::ggsave(filename = paste(plot_type, "_", x, ".png", sep = ""), bg = "white", plot = plot_list[[x]]))
+save_plot_list <- function(plot_list, plot_type = "bigram", dpi = 700){
+  lapply(names(plot_list),function(x) ggplot2::ggsave(filename = paste(plot_type, "_", x, ".png", sep = ""), dpi = dpi, bg = "white", plot = plot_list[[x]]))
 }
