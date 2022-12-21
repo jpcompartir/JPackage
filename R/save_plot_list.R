cb3b3f77770f182e797a6e7915a84e64137afa47
Jack Penzer <jp1689@my.bristol.ac.uk>
2022-12-21 22:36:25 +0000

    Update save_plot_list add fun net_income calculator

--- R/save_plot_list.R
+++ R/save_plot_list.R
@@ -8,14 +8,35 @@
 #' @param width Plot width in specified units
 #' @param height Plot width in specified units
 #' @param units the appropriate unit of measurement for plot dimensions
+#' @param dir the directory to save plots in (defaults to current working directory)
 #'
 #' @return does not return anything - saves plots to working directory
 #' @export
-#'
-save_plot_list <- function(plot_list, plot_type = "bigram", dpi = 700, width = 9, height = 6, units = c("in", "cm",
-                                                                                                                "mm", "px")){
+#' @examples
+#' \dontrun{
+#' df <- ParseR::sprinklr_export %>% janitor::clean_names()
+#' bigrams_list <- df %>% group_split(social_network) %>% map(make_bigram_viz, message, .progress = TRUE)
+#' names(bigrams_list) <- c("1", "2", "3")
+#' dir.create("test_dir")
+#' save_plot_lists(plot_list = bigrams_list, dir = "test_dir")
+#' }
+save_plot_list <- function(plot_list, dir = NULL, plot_type = "bigram", dpi = 700, width = 9, height = 6, units = c("in", "cm", "mm", "px")){
 
-  units <- match.arg(units)
 
-  lapply(names(plot_list),function(x) ggplot2::ggsave(filename = paste(plot_type, "_", x, ".png", sep = ""), width = width, height = height, units = units, dpi = dpi, bg = "white", plot = plot_list[[x]]))
+  #Make the directory the current working directory if not set
+  if(is.null(dir)){
+    dir <- paste0(getwd(), "/")
+  } else{
+    if(!is.character(dir)){ #Check that directory is a character as it should be
+      stop("dir argument should be left blank or be input as a character vector")
+    }
+  }
+  #If the dirextory does not end with a /, add one
+  if(!endsWith(dir, "/")){dir <- paste0(dir, "/")}
+
+  #Set default as "in" and if not match with input of units
+  units <- match.arg(if (missing(units)) "in" else units, c("in", "cm", "mm", "px"))
+
+  lapply(names(plot_list),function(x) ggplot2::ggsave(filename = paste(dir, plot_type, "_", x, ".png", sep = ""), width = width, height = height, units = units, dpi = dpi, bg = "white", plot = plot_list[[x]]))
 }
+
---
+++ R/zzz.R
@@ -0,0 +1,59 @@
+net_income <- function(salary, pension = TRUE, student_loan = TRUE, tax_free_allowance = 12570){
+
+  high_bracket <- 50270
+  extra_bracket <- 150000
+
+  taxable_income <- salary - tax_free_allowance
+
+
+  pension_contribution_annual <- 0
+  if(pension){
+    pension_contribution_annual <- taxable_income * 0.05
+    # pension_contribution_mensual <- pension_contribution_annual/12
+  }
+
+  student_loan_contribution_annual <- 0
+  if(student_loan){
+    student_loan_contribution_annual <- 0.05 * taxable_income
+    # student_loan_contribution_mensual <- student_loan_contribution_annual / 12
+  }
+
+  if(salary <= high_bracket){
+    basic_rate_taxable <-  salary - tax_free_allowance
+  } else{basic_rate_taxable <- high_bracket - tax_free_allowance}
+
+  higher_rate_taxable <- salary - high_bracket
+  extra_rate_taxable <- salary - extra_bracket
+
+  if(salary <= high_bracket){
+    income_tax <- 0.2 * basic_rate_taxable
+  } else if(salary > high_bracket){
+    income_tax <- (0.2 * basic_rate_taxable) + (0.4 * higher_rate_taxable)
+  } else if(salary > extra_bracket){
+    income_tax <- (0.2 * basic_rate_taxable) + (0.4 * higher_rate_taxable) + (0.45 * extra_rate_taxable)
+  }
+
+  calculate_ni <- function(salary){
+    free_allowance <- 242*52
+
+    ni_deductable <- salary - free_allowance
+
+    if(salary <= 52988){
+      ni <- ni_deductable * 0.1325
+    }else{
+      higher_portion <- 40404 * 0.1325
+
+      lower_portion <- (salary - 52988) * 0.0325
+
+      ni <- higher_portion + lower_portion
+    }
+    return(ni)
+
+  }
+
+  national_insurance <- calculate_ni(salary)
+
+  net_income <- (taxable_income - (income_tax + pension_contribution_annual + student_loan_contribution_annual + national_insurance)) + tax_free_allowance
+
+  return(net_income)
+}
