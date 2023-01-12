44c277a22e9828132feaff8933da7c1899eddafc
Jack Penzer <jp1689@my.bristol.ac.uk>
2023-01-12 19:43:07 +0000

    add match arg for type to plot_group_sentiment

--- R/plot_group_sentiment.R
+++ R/plot_group_sentiment.R
@@ -3,7 +3,7 @@
 #' @param df data frame
 #' @param group_var Grouping variable, e.g. country, topic, cluster
 #' @param sentiment_var Sentiment variable (categorical)
-#' @param type Whether the plot should be of volume or py percentage. Acccepts "percent" or "volume"
+#' @param type Whether the plot should be of volume or py percentage. Accepts "percent" or "volume"
 #' @param title The title of the plot, entered as a string.
 #' @param bar_labels Whether to add the raw volume, percentage or neither to the bars
 #'
@@ -11,34 +11,45 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' Example 1:
+#' # Example 1:
 #' plot_group_sentiment(df, group_var = platform, sentiment_var = sentiment, type = "percent")
 #'
-#' Example 2:
+#' # Example 2:
 #' plot_group_sentiment(df, group_var = topic, sentiment_var = sentiment, type = "volume")
 #' }
-plot_group_sentiment <- function(df, group_var = topic, sentiment_var = sentiment, type = "percent",title = "Grouped Sentiment Chart", bar_labels = c("None", "Percent", "Volume")){
+plot_group_sentiment <- function(df,
+                                 group_var = topic,
+                                 sentiment_var = sentiment,
+                                 type = c("percent", "volume"),
+                                 title = "Grouped Sentiment Chart", bar_labels = c("none", "percent", "volume")) {
   group_sym <- rlang::ensym(group_var)
-  bar_labels <- match.arg(if (missing(bar_labels)) "Volume" else bar_labels, c("None", "Percent", "Volume"))
+  bar_labels <- match.arg(if (missing(bar_labels)) "volume" else bar_labels, c("none", "percent", "volume"))
+  type <- match.arg(if (missing(type)) "percent" else type, c("percent", "volume"))
 
   df <- df %>%
-    dplyr::count({{group_var}}, {{sentiment_var}}) %>%
-    dplyr::add_count({{group_var}}, wt = n, name = ".total") %>%
-    dplyr::mutate(percent = n/.total * 100,
-                  percent_character = paste0(round(percent, digits = 1), "%"))
+    dplyr::count({{ group_var }}, {{ sentiment_var }}) %>%
+    dplyr::add_count({{ group_var }}, wt = n, name = ".total") %>%
+    dplyr::mutate(
+      percent = n / .total * 100,
+      percent_character = paste0(round(percent, digits = 1), "%")
+    )
 
-  if(type == "percent"){
+  if (type == "percent") {
     plot <- df %>%
-      ggplot2::ggplot(ggplot2::aes(x = reorder(!!group_sym,n), y = percent, fill = {{sentiment_var}}))+
+      ggplot2::ggplot(ggplot2::aes(x = reorder(!!group_sym, n), y = percent, fill = {{ sentiment_var }})) +
       ggplot2::geom_col() +
-      ggplot2::labs(fill = NULL, y = NULL, x = "% of Posts",
-                    title = title)
-  } else if(type == "volume"){
+      ggplot2::labs(
+        fill = NULL, y = NULL, x = "% of Posts",
+        title = title
+      )
+  } else if (type == "volume") {
     plot <- df %>%
-      ggplot2::ggplot(ggplot2::aes(x = reorder(!!group_sym, n), y = n, fill = {{sentiment_var}}))+
+      ggplot2::ggplot(ggplot2::aes(x = reorder(!!group_sym, n), y = n, fill = {{ sentiment_var }})) +
       ggplot2::geom_col() +
-      ggplot2::labs(fill = NULL, y = NULL, x = "Number of Posts",
-                    title = title)
+      ggplot2::labs(
+        fill = NULL, y = NULL, x = "Number of Posts",
+        title = title
+      )
   }
 
   plot <- plot +
@@ -48,19 +59,21 @@ plot_group_sentiment <- function(df, group_var = topic, sentiment_var = sentimen
     ggplot2::scale_x_discrete(expand = c(0, 0)) +
     ggplot2::scale_y_discrete(expand = c(0, 0))
 
-  if(bar_labels == "Percent"){
+  if (bar_labels == "Percent") {
     plot <- plot +
       ggplot2::geom_text(aes(label = percent_character),
-                         colour = "white",
-                         position = position_stack(0.5),
-                         check_overlap = TRUE)
+        colour = "white",
+        position = position_stack(0.5),
+        check_overlap = TRUE
+      )
   }
-  if(bar_labels == "Volume"){
+  if (bar_labels == "Volume") {
     plot <- plot +
       ggplot2::geom_text(aes(label = scales::comma(n)),
-                         colour = "white",
-                         position = position_stack(0.5),
-                         check_overlap = TRUE)
+        colour = "white",
+        position = position_stack(0.5),
+        check_overlap = TRUE
+      )
   }
   return(plot)
 }
